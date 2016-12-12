aa4490128a129a98de918ec21118b1e414ddc0e2
Jeferson Lima <oi_je@hotmail.com>
2016-12-11 23:49:41 -0300

    Update terminal.ino

--- terminal.ino
+++ terminal.ino
@@ -1,4 +1,5 @@
 #define MAX_COMMAND_SIZE 5000
+#define PS1 "$# > "
 
 void setup() {
   Serial.begin(9600);
@@ -19,10 +20,15 @@ void loop() {
       }
     }
 
-    comando[i] = 0;
-    Serial.print("$# > ");
-    Serial.println(comando);
-    strcpy(&comando[i], " > /dev/ttyGS0 2> /dev/ttyGS0");
-    system(comando);
+    if(i != MAX_COMMAND_SIZE){
+      comando[i] = 0;
+      Serial.print(PS1);
+      Serial.println(comando);
+      strcpy(&comando[i], " > /dev/ttyGS0 2> /dev/ttyGS0");
+      system(comando);
+    }
+    else{
+      Serial.println("[ERROR] The command entered is too long!\n" PS1);
+    }
   }
 }
